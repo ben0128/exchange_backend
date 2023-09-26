06b05b622363c0f7b92a60c66388e97c63ba9b78
ben0128 <a84012807@gmail.com>
2023-09-26 20:48:49 +0800

    change cors

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ require("dotenv").config();
 
 require("./config/mongoose");
 
-app.use("*", cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
