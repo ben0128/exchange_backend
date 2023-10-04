7defee19063f7e5c698da93f4f6263b2acd7f194
Banwind <a84012807@gmail.com>
2023-10-04 17:03:27 +0800

    fix login bugs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const cors = require("cors");
-// const flash = require("connect-flash");
+const flash = require("connect-flash");
 const methodOverride = require("method-override");
 const router = require("./routes/index");
 const app = express();
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-// app.use(flash());
+app.use(flash());
 app.use(passport.initialize());
 app.use("/api", router);
 
